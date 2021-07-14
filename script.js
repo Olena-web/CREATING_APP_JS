af36a78c4694c421d401e007366e5165cc58164d
Olena Honcharuk <gasmin07@gmail.com>
2021-07-14 23:21:39 +0300

    used function

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-// "use strict";
+"use strict";
 // const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 // const personalMovieDB = {
 //   count: numberOfFilms,
@@ -55,8 +55,17 @@
 
 // Код возьмите из предыдущего домашнего задания
 
-"use strict";
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+//"use strict";
+let numberOfFilms;
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+start();
+
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -65,59 +74,55 @@ const personalMovieDB = {
   privat: false,
 };
 
-// for (let i = 0; i < 2; i++) {
-//   const a = prompt("Один из последних просмотренных фильмов?", "");
-//   const b = prompt("На сколько оцените его?", "");
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", "");
+    const b = prompt("На сколько оцените его?", "");
 
-//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-//     personalMovieDB.movies[a] = b;
-//     console.log("done");
-//   } else {
-//     console.log("error");
-//     i--;
-//   }
-// }
-
-//while
-// let i = 0;
-// while (i < 2) {
-//   i++;
-//   const a = prompt("Один из последних просмотренных фильмов?", "");
-//   const b = prompt("На сколько оцените его?", "");
-
-//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-//     personalMovieDB.movies[a] = b;
-//     console.log("done");
-//   } else {
-//     console.log("error");
-//     i--;
-//   }
-// }
-
-//do
-let i = 0;
-do {
-  i++;
-  const a = prompt("Один из последних просмотренных фильмов?", "");
-  const b = prompt("На сколько оцените его?", "");
-
-  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log("done");
-  } else {
-    console.log("error");
-    i--;
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("done");
+    } else {
+      console.log("error");
+      i--;
+    }
   }
-} while (i < 2);
-
-if (personalMovieDB.count < 10) {
-  console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-  console.log("Вы киноман");
-} else {
-  console.log("Произошла ошибка");
 }
+//rememberMyFilms();
 
-console.log(personalMovieDB);
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+*/
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+//detectPersonalLevel();
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+}
+writeYourGenres();
---
+++ theory.js
@@ -0,0 +1,60 @@
+//while
+// let i = 0;
+// while (i < 2) {
+//   i++;
+//   const a = prompt("Один из последних просмотренных фильмов?", "");
+//   const b = prompt("На сколько оцените его?", "");
+
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("done");
+//   } else {
+//     console.log("error");
+//     i--;
+//   }
+// }
+
+//do
+//let i = 0;
+// do {
+//   i++;
+//   const a = prompt("Один из последних просмотренных фильмов?", "");
+//   const b = prompt("На сколько оцените его?", "");
+
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("done");
+//   } else {
+//     console.log("error");
+//     i--;
+//   }
+// } while (i < 2);
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//   }
+//   showFirstMessage("Hello, World!");
+//   const logger = function () {
+//     console.log("hello");
+//   };
+//   logger();
+
+//   const calc = (a, b) => a + b;
+
+//   //methods for strings
+//   //изменение регистра строки
+//   const str = "test";
+//   console.log(str.toUpperCase());
+
+//   let fruit = "Some fruit";
+//   console.log(fruit.indexOf("fruit"));
+//   console.log(fruit.indexOf("q"));
+//   const logg = "hello, world";
+//   console.log(logg.slice(-5));
+
+//   const num = 12.2;
+//   console.log(Math.round(num));
+
+//   const test = "12.2px";
+//   console.log(parseInt(test));
+//   console.log(parseFloat(test));
